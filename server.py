e5b00923fb73d097827dc6d61897e948b6f1d899
bbxiuu <bbxiuu@github.com>
2022-06-30 06:49:07 +0800

    server

--- server.py
+++ server.py
@@ -1,33 +1,71 @@
 import socket
-import os
+import select
+import sys
+
 from _thread import *
 
-ServerSocket = socket.socket()
-host = ''
-port = 8889
-ThreadCount = 0
-try:
-    ServerSocket.bind((host, port))
-except socket.error as e:
-    print(str(e))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-print('Waiting for a Connection..')
-ServerSocket.listen(5)
+if len(sys.argv) != 3:
+    print ("Correct usage: script, IP address, port number")
+    exit()
 
-def threaded_client(connection):
-    connection.send(str.encode('Welcome to the Server\n'))
+IP_address = str(sys.argv[1])
+
+Port = int(sys.argv[2])
+
+server.bind((IP_address, Port))
+
+server.listen(100)
+
+list_of_clients = []
+
+def clientthread(conn, addr):
+
+    conn.send("Welcome to this chatroom!")
+
     while True:
-        data = connection.recv(2048)
-        reply = 'Server Says: ' + data.decode('utf-8')
-        if not data:
-            break
-        connection.sendall(str.encode(reply))
-    connection.close()
+            try:
+                message = conn.recv(2048)
+                if message:
+
+                    print ("<" + addr[0] + "> " + message)
+
+                    message_to_send = "<" + addr[0] + "> " + message
+                    broadcast(message_to_send, conn)
+
+                else:
+                    """message may have no content if the connection
+                    is broken, in this case we remove the connection"""
+                    remove(conn)
+
+            except:
+                continue
+
+def broadcast(message, connection):
+    for clients in list_of_clients:
+        if clients!=connection:
+            try:
+                clients.send(message)
+            except:
+                clients.close()
+
+                remove(clients)
+
+def remove(connection):
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
 
 while True:
-    Client, address = ServerSocket.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    start_new_thread(threaded_client, (Client, ))
-    ThreadCount += 1
-    print('Thread Number: ' + str(ThreadCount))
-ServerSocket.close()
+
+    conn, addr = server.accept()
+
+    list_of_clients.append(conn)
+
+    print (addr[0] + " connected")
+
+    start_new_thread(clientthread,(conn,addr))
+
+conn.close()
+server.close()
