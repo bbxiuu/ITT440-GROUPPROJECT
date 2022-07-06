10603b3cef9d4cb060377907e44f0b893fa8ba4f
Azrifaiz <2021843172@student.uitm.edu.my>
2022-07-06 07:06:56 +0000

    update client2

--- client2.py
+++ client2.py
@@ -1,36 +1,41 @@
 import socket
 import threading
+from datetime import datetime
+
+e = datetime.now()
 
 # Choosing Nickname
 nickname = input("Choose your nickname: ")
 
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.6.4', 8889))
+client.connect(('192.168.248.2', 8889))
 
 # Listening to Server and Sending Nickname
 def receive():
 
      while True:
             try:
-                # Receive Message From Server
-                # If 'NICK' Send Nickname
-                message = client.recv(1024).decode('ascii')
-                if message == 'NICK':
-                   client.send(nickname.encode('ascii'))
-                else:
-                   print(message)
+                 # Receive Message From Server
+                 # If 'NICK' Send Nickname
+                  message = client.recv(1024).decode('ascii')
+                  if message == 'NICK':
+                     client.send(nickname.encode('ascii'))
+                  else:
+                     print(message)
             except:
-                # Close Connection When Error
-                print("An error occured!")
-                client.close()
-                break
+                 # Close Connection When Error
+                 print("An error occured!")
+                 client.close()
+                 break
 
 # Sending Messages To Server
 def write():
     while True:
                message = f'{nickname}: {input (" " )}'
                client.send(message.encode('ascii'))
+               current = e.strftime("%H:%M:%S")
+               print(current)
 
 # Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
